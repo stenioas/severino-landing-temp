00818ea75922738598e09ff31f58d60222bdf080
Stenio Silveira <stenioas@gmail.com>
2025-07-08 16:43:41 -0300

    feat(api): add metodo para salvar email

---
+++ src/api/newsletterApi.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'https://severino.onrender.com/emailSubscribe';
+
+export const saveNewsletterEmail = async (email: string) => {
+  try {
+    const response = await axios.post(
+      API_BASE_URL,
+      { email },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        withCredentials: true,
+      },
+    );
+    return response.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      throw error.response?.data || error.message;
+    }
+    throw error;
+  }
+};
